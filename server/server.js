35103c261834ab745884490d2e0ea151c6795dd2
mdsaquib5 <mdsaquib5540@gmail.com>
2025-08-01 15:24:14 +0530

    network erro

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allow Multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://green-cart-taupe.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://affrontend.vercel.app'];
 
 app.post('/stripe', express.raw({type: "application/json"}), stripWebHooks);
 
